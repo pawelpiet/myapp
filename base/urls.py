f0dbd9e83889e6797dfa06baef10cddccd58cf26
s18793 <100522624+pawelpiet@users.noreply.github.com>
2022-03-09 18:46:00 +0100

    add login

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('',views.home, name="home"),
     path('race/<str:pk>/',views.race, name="race"),
     path('land/<str:pk>/',views.land, name="land"),
--- base/views.py
+++ base/views.py
@@ -1,17 +1,58 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Land, Race, Hero, Room
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Land, Race, Hero, Room, User
 from .forms import RoomForm
 # Create your views here.
 
 
+def loginPage(request):
+    
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request,'User or password does not exist')
+
+    context = {}
+    return render(request,'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 
 def home(request):
+    
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(host__username__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
     lands = Land.objects.all()
-    rooms = Room.objects.all()
-    print('dsd')
-    context = {'lands': lands, 'rooms': rooms}
+    users = User.objects.all()
+    room_count = rooms.count()
+    context = {'lands': lands, 'rooms': rooms, 'users': users, 'room_count': room_count}
     return render(request,'base/home.html', context) 
 
 def race(request,pk):
@@ -34,7 +75,7 @@ def room(request,pk):
     context = {'room':room}
     return render(request,'base/room.html', context)
 
-
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -48,7 +89,7 @@ def createRoom(request):
     context={'form': form}
     return render(request,'base/room_form.html', context)
 
-
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -63,7 +104,7 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request,'base/room_form.html', context)
 
-
+@login_required(login_url='login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
